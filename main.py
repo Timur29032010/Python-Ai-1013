bd21a065d6ac5046bb0bf0808586240d9ea56cda
Timur29032010 <tumyrdychakivski@gmail.com>
2023-10-30 20:34:59 +0200

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print("Hello AI")
+class Dog:
+
+   def __init__(self,name):
+
+       self.name = name
+
+       self.hungry = True
+
+       self.tired = True
+
+       self.happy = False
+
+
+   def food(self):
+
+       if self.hungry:
+
+           print(f"{self.name} Hungry and crazy.")
+
+           self.hungry = False
+
+       else:
+
+           print(f"{self.name} is not hungry.")
+
+
+   def sleep(self):
+
+       if self.tired:
+
+           print(f"{self.name} has gone to sleep.")
+
+           self.tired = False
+
+       else:
+
+           print(f"{self.name} is not tired.")
+
+
+   def play(self):
+
+       if self.happy:
+
+           print(f"{self.name} is already playing.")
+
+       else:
+
+           print(f"{self.name} is playing now.")
+
+           self.happy = True
