5d6307eaf01ebafa4065b21c9d2d636d5b43b64e
1013 <jekakrv209@gmail.com>
2023-11-02 15:39:04 +0200

    Dz

--- Lesson3/sims.py
+++ Lesson3/sims.py
@@ -1,59 +1,24 @@
-import random
-
-class Human:
-    def __init__(self, name="Human",
-                 job=None, home=None,
-                 car=None):
-        self.name = name
-        self.money = 100
-        self.gladness = 50
-        self.satiety = 50
-        self.job = job
-        self.car = car
-        self.home = home
-def get_home(self):
- pass
-
-def get_car(self):
- pass
-
-def get_job(self):
- pass
-
-def eat(self):
- pass
-
-def work(self):
- pass
-
-def shopping(self, manage):
- pass
-
-def chill(self):
- pass
-
-def clean_home(self):
- pass
-
-def to_repair(self):
- pass
-
-def days_indexes(self, day):
- pass
-
-def is_alive(self):
- pass
-
-def live(self, day):
- pass
-
-class Auto:
-    def __init__(self, brand_list):
-         self.brand=random.choice(list (brand_list))
-         self.fuel=brand_list[self.brand]["fuel"]
-         self.strength = brand_list[self.brand]["strength"]
-         self.consumption=brand_list[self.brand]["consumption"]
-
-
-
-
+class House:
+ def __init__(self):
+  self.mess = 0
+  self.food = 0
+job_list = {
+ "Java developer":
+ {"salary":50, "gladness_less": 10 },
+ "Python developer":
+ {"salary":40, "gladness_less": 3 },
+ "C++ developer":
+ {"salary":45, "gladness_less": 25 },
+ "Rust developer":
+ {"salary":70, "gladness_less": 1 },
+ }
+brands_of_car = {
+ "BMW":{"fuel":100, "strength":100,
+ "consumption": 6},
+ "Lada":{"fuel":50, "strength":40,
+ "consumption": 10},
+ "Volvo":{"fuel":70, "strength":150,
+ "consumption": 8},
+ "Ferrari":{"fuel":80, "strength":120,
+ "consumption": 14},
+ }
